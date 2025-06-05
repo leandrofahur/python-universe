3de5927a5b32db59cd823e833dd8ad083f2cbdc8
Leandro Miranda Fahur Machado <leandrofahur@gmail.com>
2025-06-05 12:15:41 -0700

    added matrix

--- basic/data_types.py
+++ basic/data_types.py
@@ -108,6 +108,17 @@ print([1, 2, 3].pop()) # 3
 print([1, 2, 3].remove(2)) # [1, 3]
 print([1, 2, 3].reverse()) # [3, 2, 1]
 
+# Matrix:
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(matrix)
+print(matrix[0]) # [1, 2, 3]
+print(matrix[2][1]) # 8
+print(matrix[0][::-1]) # [3, 2, 1]
+
 # Tuples:
 print("\nTuples:")
 print((1, 2, 3))
