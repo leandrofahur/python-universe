6a18c319718730ada5ccde7927a261bf5c11589d
Leandro Miranda Fahur Machado <leandrofahur@gmail.com>
2025-06-07 14:18:27 -0700

    updated

--- basic/oop.py
+++ basic/oop.py
@@ -24,9 +24,29 @@ class PlayerCharacter:
     def adding_things2(num1, num2):
         return num1 + num2
     
-player1 = PlayerCharacter('Peter', 10)
-print(player1.getName())
-print(player1.getAge())
+# player1 = PlayerCharacter('Peter', 10)
+# print(player1.getName())
+# print(player1.getAge())
 
-print(PlayerCharacter.adding_things(12, 13))
-print(PlayerCharacter.adding_things2(2, 3))
+# print(PlayerCharacter.adding_things(12, 13))
+# print(PlayerCharacter.adding_things2(2, 3))
+
+class User:
+    def __init__(self, email):
+        self._email = email
+
+    def sign_in(self):
+        print('logged in')
+
+class Wizard(User):
+    def __init__(self, name, power, email):
+        super().__init__(email)
+        self._name = name
+        self._power = power
+
+class Archer(User):
+    def __init__(self, name, num_arrows, email):
+        super().__init__(email)
+        self._name = name
+        self._num_arrows = num_arrows
+        
