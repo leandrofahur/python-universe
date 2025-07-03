8f43c0f7a5435ca440059f4df64a3465e349c5f7
Leandro Miranda Fahur Machado <leandrofahur@gmail.com>
2025-07-03 14:25:49 -0700

    update

--- basic/functional-programming/2_high-order-functions.py
+++ basic/functional-programming/2_high-order-functions.py
@@ -3,11 +3,20 @@
 def multiply_by_two(item):
     return item * 2
 
-print(map(multiply_by_two,[1,2,3]))
+# print(map(multiply_by_two,[1,2,3]))
+print("map")
 print(list(map(multiply_by_two,[1,2,3])))
 # print(list(map(lambda item: item * 2,[1,2,3])))
 
+# filter(func,iterable)
+def check_odd(item):
+    return item % 2 != 0
+
+print("\nfilter")
+print(list(filter(check_odd,[1,2,3])))
+
 # zip(iterable1,iterable2)
+print("\nzip")
 print(list(zip([1,2,3],[4,5,6])))
 
 # reduce
